ae1faa9c24053ce6b8e7c7c2a2a3d2e7d00403b0
Jivjot <jivjots@sfu.ca>
2015-12-02 14:43:15 -0800

    random script

---
+++ random.sh
@@ -0,0 +1,25 @@
+if [ $# -ne 3 ]; then
+  exit 1
+fi
+mkdir $1
+mkdir $1/Train
+mkdir $1/Val
+mkdir $1/Test
+for d in ` ls genres`
+do 
+for f in  `ls genres/$d`
+do
+path=genres/$d/$f
+rNo=$(( RANDOM % 100 ))
+echo $rNo
+if [ $rNo -lt $2 ]
+then 
+cp $path $1/Train
+elif [ $rNo -lt $3 ]
+then
+cp $path $1/Val
+else
+cp $path $1/Test
+fi
+done
+done
