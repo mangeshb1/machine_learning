c53f3b0b07d4ed0248861c3adad125c3a901e9da
Jivjot <jivjots@sfu.ca>
2015-12-03 14:36:01 -0800

    random feature extraction

---
+++ makeFeatures.sh
@@ -0,0 +1,4 @@
+./mkcollection -c train.mf  "/home/jivjot/git/musicGenre/random/Train" 
+./bextract -n -fe -mfcc "train.mf" -w train.arff    ##overall features 
+java weka.core.converters.CSVSaver -i MARSYAS_EMPTYtrain.arff -o train.csv
+cat train.csv | rev | cut -d"," -f1 --complement|rev > train.csv
--- random.sh
+++ random.sh
@@ -1,6 +1,10 @@
 if [ $# -ne 3 ]; then
   exit 1
 fi
+rm labels.txt
+touch labels.txt
+rm labelsN.txt
+touch labelsN.txt
 mkdir $1
 mkdir $1/Train
 mkdir $1/Val
@@ -11,7 +15,10 @@ for f in  `ls genres/$d`
 do
 path=genres/$d/$f
 rNo=$(( RANDOM % 100 ))
-echo $rNo
+labelName=`echo $path| cut -d"/" -f2`
+echo $labelName >> labels.txt
+./labelMap.sh $labelName >> labelsN.txt 
+echo $path
 if [ $rNo -lt $2 ]
 then 
 cp $path $1/Train
