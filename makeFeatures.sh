6a88398fd3791291e782d3698aafcae87cbf2aeb
Jivjot <jivjots@sfu.ca>
2015-12-03 15:00:03 -0800

    dataset

--- makeFeatures.sh
+++ makeFeatures.sh
@@ -2,4 +2,6 @@
 #./bextract -n -fe -mfcc "train.mf" -w train.arff    ##overall features 
 ./bextract -n -sv -mfcc "train.mf" -w train.arff    ##overall features 
 java weka.core.converters.CSVSaver -i MARSYAS_EMPTYtrain.arff -o train.csv
-cat train.csv | rev | cut -d"," -f1 --complement|rev > train.csv
+cat train.csv | rev | cut -d"," -f1 --complement|rev > train1.csv
+cp train1.csv train.csv
+rm train1.csv
