13e7a1ec66165d6c20c488b2050c5fb2ccc7b176
Jivjot <jivjots@sfu.ca>
2015-12-03 16:22:09 -0800

    new files

--- random.sh
+++ random.sh
@@ -1,10 +1,21 @@
 if [ $# -ne 3 ]; then
   exit 1
 fi
-rm labels.txt
-touch labels.txt
-rm labelsN.txt
-touch labelsN.txt
+labelsTrain=labelsTrain.txt
+labelsNTrain=labelsNTrain.txt
+labelsVal=labelsVal.txt
+labelsNVal=labelsNVal.txt
+labelsTest=labelsTest.txt
+labelsNTest=labelsNTest.txt
+
+rm $labelsTrain
+rm $labelsNTrain
+rm $labelsVal
+rm $labelsNVal
+rm $labelsTest
+rm $labelsNTest
+
+
 mkdir $1
 mkdir $1/Train
 mkdir $1/Val
@@ -16,17 +27,21 @@ do
 path=genres/$d/$f
 rNo=$(( RANDOM % 100 ))
 labelName=`echo $path| cut -d"/" -f2`
-echo $labelName >> labels.txt
-./labelMap.sh $labelName >> labelsN.txt 
 echo $path
 if [ $rNo -lt $2 ]
 then 
 cp $path $1/Train
+echo $labelName >> $labelsTrain
+./labelMap.sh $labelName >> $labelsNTrain
 elif [ $rNo -lt $3 ]
 then
 cp $path $1/Val
+echo $labelName >> $labelsVal
+./labelMap.sh $labelName >> $labelsNVal
 else
 cp $path $1/Test
+echo $labelName >> $labelsTest
+./labelMap.sh $labelName >> $labelsNTest
 fi
 done
 done
