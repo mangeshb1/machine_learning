2ceaf64818cfe913c4520396400a7349d256e5a3
Jivjot <jivjots@sfu.ca>
2015-12-03 17:04:55 -0800

    new training files

--- makeFeatures.sh
+++ makeFeatures.sh
@@ -1,7 +1,15 @@
 ./mkcollection -c train.mf  "/home/jivjot/git/musicGenre/random/Train" 
 #./bextract -n -fe -mfcc "train.mf" -w train.arff    ##overall features 
-./bextract -n -sv -mfcc "train.mf" -w train.arff    ##overall features 
+#./bextract  -n -sv -mfcc "train.mf" -w train.arff    ##overall features 
+./bextract  -n -sv  "train.mf" -w train.arff    ##overall features 
+#./bextract  -n -fe "train.mf" -w train.arff    ##overall features 
 java weka.core.converters.CSVSaver -i MARSYAS_EMPTYtrain.arff -o train.csv
 cat train.csv | rev | cut -d"," -f1 --complement|rev > train1.csv
 cp train1.csv train.csv
 rm train1.csv
+cat MARSYAS_EMPTYtrain.arff |grep filename | cut -d"." -f1| rev |cut -d"/" -f1|rev > labelsTrain.txt
+rm labelsNTrain.txt
+for d in `cat labelsTrain.txt`
+do 
+./labelMap.sh $d >> labelsNTrain.txt
+done
--- random.sh
+++ random.sh
@@ -1,19 +1,7 @@
 if [ $# -ne 3 ]; then
   exit 1
 fi
-labelsTrain=labelsTrain.txt
-labelsNTrain=labelsNTrain.txt
-labelsVal=labelsVal.txt
-labelsNVal=labelsNVal.txt
-labelsTest=labelsTest.txt
-labelsNTest=labelsNTest.txt
 
-rm $labelsTrain
-rm $labelsNTrain
-rm $labelsVal
-rm $labelsNVal
-rm $labelsTest
-rm $labelsNTest
 
 
 mkdir $1
@@ -31,17 +19,11 @@ echo $path
 if [ $rNo -lt $2 ]
 then 
 cp $path $1/Train
-echo $labelName >> $labelsTrain
-./labelMap.sh $labelName >> $labelsNTrain
 elif [ $rNo -lt $3 ]
 then
 cp $path $1/Val
-echo $labelName >> $labelsVal
-./labelMap.sh $labelName >> $labelsNVal
 else
 cp $path $1/Test
-echo $labelName >> $labelsTest
-./labelMap.sh $labelName >> $labelsNTest
 fi
 done
 done
