6188fd21522e3ba7a4c610c8054a582cf338235b
Jivjot <jivjots@sfu.ca>
2015-12-03 17:47:10 -0800

    new feature file

--- makeFeatures.sh
+++ makeFeatures.sh
@@ -1,15 +1,18 @@
 ./mkcollection -c train.mf  "/home/jivjot/git/musicGenre/random/Train" 
 #./bextract -n -fe -mfcc "train.mf" -w train.arff    ##overall features 
 #./bextract  -n -sv -mfcc "train.mf" -w train.arff    ##overall features 
-./bextract  -n -sv  "train.mf" -w train.arff    ##overall features 
+#./bextract  -n -sv  "train.mf" -w train.arff    ##overall features 
 #./bextract  -n -fe "train.mf" -w train.arff    ##overall features 
 java weka.core.converters.CSVSaver -i MARSYAS_EMPTYtrain.arff -o train.csv
 cat train.csv | rev | cut -d"," -f1 --complement|rev > train1.csv
 cp train1.csv train.csv
 rm train1.csv
-cat MARSYAS_EMPTYtrain.arff |grep filename | cut -d"." -f1| rev |cut -d"/" -f1|rev > labelsTrain.txt
-rm labelsNTrain.txt
+echo Label > labelsTrain.txt
+echo Label > labelsNTrain.txt
+
+cat MARSYAS_EMPTYtrain.arff |grep filename | cut -d"." -f1| rev |cut -d"/" -f1|rev >> labelsTrain.txt
 for d in `cat labelsTrain.txt`
 do 
 ./labelMap.sh $d >> labelsNTrain.txt
 done
+paste train.csv labelsNTrain.txt -d","> JoinedTrain.csv
